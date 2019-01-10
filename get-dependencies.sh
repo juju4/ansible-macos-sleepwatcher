a3df0d1514429ba336d6de1974331a494a712362
juju4 <juju4@users.noreply.github.com>
2019-01-10 20:17:59 +0000

    galaxy lint, naming

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,8 +12,7 @@ fi
 #[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.macos-sleepwatcher ] && ln -s ansible-macos-sleepwatcher $rolesdir/juju4.macos-sleepwatcher
-[ ! -e $rolesdir/juju4.macos-sleepwatcher ] && cp -R $rolesdir/ansible-macos-sleepwatcher $rolesdir/juju4.macos-sleepwatcher
+[ ! -e $rolesdir/juju4.macos_sleepwatcher ] && cp -R $rolesdir/ansible-macos-sleepwatcher $rolesdir/juju4.macos_sleepwatcher
 
 ## don't stop build on this script return code
 true
-
